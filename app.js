503b10b56745c6bc769b8e9c999502094435e709
Djaouel <djaouel.nahla@hotmail.fr>
2022-09-26 21:08:39 +0200

    add index

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
 
-// middleware
-app.use ((req, res, next) => {
-    console.log("Request received");
-    next();
-});
+//  //  middleware
+ 
+//  app.use ((req, res, next) => {
+//      console.log("Request received");
+//      next();
+//  });
 
-app.use ((req, res, next) => {
-    res.status(201);
-    next();
-});
+//  app.use ((req, res, next) => {
+//      res.status(201);
+//      next();
+//  });
 
-app.use((req, res, next) => {
-    res.json( { message :"votre requête a bien été reçue !" });
-    next();
-});
+//  app.use((req, res, next) => {
+//      res.json( { message :"votre requête a bien été reçue !" });
+//      next();
+//  });
 
-app.use((req, res, next) => {
-    console.log("Response sent successfully");
-});
+//  app.use((req, res, next) => {
+//      console.log("Response sent successfully");
+//  });
 
 
 
-module.exports = app;
+
+// module.exports = app;
